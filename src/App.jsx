2cbc542cfce3d972fb9b2e82284a8904a3b4680f
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-30 19:44:31 +0530

    Added QR code component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import { useState } from "react"
 import QRCode from "react-qr-code"
 
 function App() {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
   return (
     <div>
       <h1>QR code Generator</h1>
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          name="qr-code"
+          value={input}
+          placeholder="Enter value to genrate QR code"
+          onChange={(e) => e.target.value}
+        />
+
         <button>
           Generate
         </button>
       </div>
       <div>
-        generated QR
+        <QRCode id="qr-code-value" value={qrCode}/>
       </div>
     </div>
   )
